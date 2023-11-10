9dfdc0a2b143fab4f96f221d07182378d79244f7
ChalakaK94 <karunarathne.cp@gmail.com>
2023-11-10 15:30:24 +0530

    toggle list box on both lists

--- src/components/StudiedList.jsx
+++ src/components/StudiedList.jsx
@@ -1,11 +1,20 @@
+import { useState } from "react";
 import BookReadSummary from "./BookReadSummary";
 import BooksReadList from "./BooksReadList";
 
 export default function StudiedList({booksRead}){
+    const [isOpen, setIsOpen] = useState(true)
     return (
         <div className="box">
-            <BookReadSummary booksRead={booksRead} />
+            <div style={{textAlign: 'end', backgroundColor:'#eee'}}>
+                <button className="btn-toggle" onClick={()=> setIsOpen((open)=> !open)}>
+                    {isOpen ? '-' : '+'}</button>
+            </div>
+            {isOpen &&  <>
+                <BookReadSummary booksRead={booksRead} />
             <BooksReadList booksRead={booksRead} />
+            </> }
+            
         </div>
     )
 }
