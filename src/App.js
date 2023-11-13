d88661f6c713e9bdc025f78e8b5192a4b874538e
ChalakaK94 <karunarathne.cp@gmail.com>
2023-11-13 22:17:58 +0530

    hadling api error and implement loading compoenent

--- src/App.js
+++ src/App.js
@@ -9,42 +9,9 @@ import BooksList from './components/BooksList';
 import BookReadSummary from "./components/BookReadSummary";
 import BooksReadList from "./components/BooksReadList";
 import FormatBookResponse from './service/FormatBookResponse';
-const Books = [
-  {
-    isbn: '9788129112859',
-    title: 'I BOUGHT THE MONKS FERRARI',
-    rating: 4.3,
-    year: 2001,
-    publisher: 'Rupa Publications India',
-    image: 'https://covers.openlibrary.org/b/id/6903838-M.jpg'
-  },
-  {
-    isbn: '9780618263225',
-    title: 'The Lord of the Rings',
-    rating: 4.3,
-    year: 2004,
-    publisher: 'HarperCollins Publishers',
-    image: 'https://covers.openlibrary.org/b/id/393992-M.jpg'
-  },
-  {
-    isbn: '9780984221233',
-    title: 'A Python Book',
-    rating: 4.3,
-    year: 2006,
-    publisher: 'Platypus Global Media"',
-    image:
-      'http://books.google.com/books/content?id=1FL-ygAACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api'
-  },
-  {
-    isbn: '9781521546185',
-    title: 'React. Js Book',
-    rating: 4.3,
-    year: 2008,
-    publisher: 'Packt Publishers',
-    image:
-      'http://books.google.com/books/content?id=e_l9zQEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api'
-  }
-];
+import Loader from './components/Loader';
+import ErrorMessage from './components/ErrorMessage';
+
 
 const BookRead = [
   {
@@ -88,17 +55,30 @@ const KEY ='AIzaSyDSwK1KQRyal2ddUwrnOyYoDJP467Wc48c';
 
 
 function App() {
-  const [booksData,setBooksData] = useState(Books)
+  const [booksData,setBooksData] = useState([])
   const [booksReadData,setBooksReadData] = useState(BookRead)
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState('')
 
   async function fetchPosts(){
-    const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=monk+ferarri&key=${KEY}`);
+    try{
+    setIsLoading(true);
+    const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=i&key=${KEY}`);
     const data = await response.json();
+    if(!data.items) throw new Error ('No Books Data Avilable!');
     setBooksData(FormatBookResponse(data))
+    setIsLoading(false)
+    }
+    catch (error){
+      setIsLoading(false)
+      setError(error.message);
+      console.log(error.message);
+      
+    }
   }
 
   useEffect(()=>{
-    fetchPosts()
+    fetchPosts() 
   },[])
 
  
@@ -109,7 +89,9 @@ function App() {
     </Navbar>
     <Main>
       <ListBox >
-        <BooksList booksData={booksData}/>
+        {isLoading && <Loader/> }
+        {!isLoading && !error &&  <BooksList booksData={booksData}/> }
+        {error && <ErrorMessage message={error}/> }
       </ListBox>
       <ListBox>
             <BookReadSummary />
---
+++ src/components/ErrorMessage.jsx
@@ -0,0 +1,5 @@
+export default function ErrorMessage({message}){
+    return(
+        <div style={{ textAlign: 'center', marginTop:'20px'}}>{message}</div>
+    )
+}
