c4dc9e984f7cce25a6d8004cfd7093dace0078f6
ChalakaK94 <karunarathne.cp@gmail.com>
2023-11-15 21:41:29 +0530

    in search box add curser  on mount

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,5 +1,33 @@
+import { useEffect, useRef } from "react"
+
 export default function Search({query, setQuery}){
+    const inputRef = useRef(null);
+
+    useEffect(() => {
+        function callBack(e){
+            if (e.code === 'Enter') {
+                const activeElement = document.activeElement
+                inputRef.current.focus();
+                if(activeElement !== inputRef.current){
+                setQuery('')
+
+                }
+            }
+        }
+        document.addEventListener('keydown', callBack)
+       
+        inputRef.current.focus();
+        return ()=>{
+            document.removeEventListener('keydown', callBack)
+        }
+    }, []);
+
+
     return (
-        <input type='text' placeholder="Search Books" value={query} onChange={e=> setQuery(e.target.value)}/>
+        <input type='text' 
+            placeholder="Search Books..." 
+            value={query} 
+            ref={inputRef}
+            onChange={e=> setQuery(e.target.value)}/>
     )
 }
